263113c7453367ab999a46faaf9a66a39ad67567
Brian Onoszko <119524154+BrianOnoszko@users.noreply.github.com>
2024-12-02 11:29:34 +0100

    Update get_worked_lessons_this_month.sql

--- task3/get_worked_lessons_this_month.sql
+++ task3/get_worked_lessons_this_month.sql
@@ -5,7 +5,7 @@ CREATE VIEW lessons_worked AS
 		COUNT(lesson.id) AS "No of Lessons"
 	FROM instructor
 	JOIN person ON instructor.id=person.id
-	LEFT OUTER JOIN lesson ON instructor.id=lesson.instructor_id
+	JOIN lesson ON instructor.id=lesson.instructor_id
 	WHERE DATE_PART('month', lesson.time_slot)=DATE_PART('month',CURRENT_DATE) AND
 		DATE_PART('year', lesson.time_slot)=DATE_PART('year',CURRENT_DATE)
 	GROUP BY person.id, instructor.id;
